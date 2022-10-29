9749bd96ea8c363189d197f6966ddbb7f6f06ae1
Campbell Barton <ideasman42@gmail.com>
2022-10-29 21:44:48 +1100

    Update scroll/clamp utility

--- stackexchange-scroll-and-clamp.el
+++ stackexchange-scroll-and-clamp.el
@@ -44,43 +44,68 @@ Argument N the number of lines, passed to `forward-line'."
   ;; Needed so `temporary-goal-column' is respected in the future.
   (setq this-command 'line-move))
 
+(defmacro stackexchange-scroll-and-clamp--with-evil-visual-mode-hack (&rest body)
+  "Execute BODY with the point not restricted to line limits.
+
+This is needed so the point is not forced to line bounds
+even when in evil visual line mode."
+  `(let ((mark-found nil))
+     (when (and (fboundp 'evil-visual-state-p)
+                (funcall 'evil-visual-state-p)
+                (fboundp 'evil-visual-type)
+                (eq (funcall 'evil-visual-type) 'line)
+                (boundp 'evil-visual-point))
+       (let ((mark (symbol-value 'evil-visual-point)))
+         (when (markerp mark)
+           (setq mark-found mark))))
+     (unwind-protect
+         (progn
+           (when mark-found
+             (goto-char (marker-position mark-found)))
+           ,@body)
+       (when mark-found
+         (set-marker mark-found (point))))))
+
+
 ;;;###autoload
 (defun stackexchange-scroll-and-clamp-up-command ()
   (interactive)
-  (let ((height (window-height)))
+  (stackexchange-scroll-and-clamp--with-evil-visual-mode-hack
+   (let ((height (window-height)))
 
-    ;; Move point.
-    (stackexchange-scroll-and-clamp--forward-line height)
+     ;; Move point.
+     (stackexchange-scroll-and-clamp--forward-line height)
 
-    ;; Move window.
-    (set-window-start
+     ;; Move window.
+     (set-window-start
       (selected-window)
       (min
-        (save-excursion ;; new point.
-          (forward-line (- (/ height 2)))
-          (point))
-        (save-excursion ;; max point.
-          (goto-char (point-max))
-          (beginning-of-line)
-          (forward-line (- (- height (+ 1 (* 2 scroll-margin)))))
-          (point)))))
+       (save-excursion ;; new point.
+         (forward-line (- (/ height 2)))
+         (point))
+       (save-excursion ;; max point.
+         (goto-char (point-max))
+         (beginning-of-line)
+         (forward-line (- (- height (+ 1 (* 2 scroll-margin)))))
+         (point))))))
   (redisplay))
 
 ;;;###autoload
 (defun stackexchange-scroll-and-clamp-down-command ()
   (interactive)
-  (let* ((height (window-height)))
+  (stackexchange-scroll-and-clamp--with-evil-visual-mode-hack
+   (let* ((height (window-height)))
 
-    ;; Move point.
-    (stackexchange-scroll-and-clamp--forward-line (- height))
-    (setq this-command 'line-move)
+     ;; Move point.
+     (stackexchange-scroll-and-clamp--forward-line (- height))
+     (setq this-command 'line-move)
 
-    ;; Move window.
-    (set-window-start
+     ;; Move window.
+     (set-window-start
       (selected-window)
       (save-excursion ;; new point.
         (forward-line (- (/ height 2)))
-        (point))))
+        (point)))))
   (redisplay))
 
 (provide 'stackexchange-scroll-and-clamp)
